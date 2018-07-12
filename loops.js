e3bae44eecdab84925bf29bc8668705740efecf8
Alec Staszewski <astaszew@gmail.com>
2018-07-12 16:22:44 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(arr1){
+  for(i = 0; i > 25; i++){
+    if(i === 1) {
+      return "I am 1 strange loop";
+    }
+    else{
+      return `I am ${i} strange loops.`;
+    }
+  }
+  return arr1;
+}
